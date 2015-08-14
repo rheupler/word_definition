d76011dee7e358e899910141b2956e9b96be69de
Ryan Heupler <Rheupler@gmail.com>
2015-08-14 11:54:49 -0700

    specs passing. will return added word to array after submission

--- spec/word_definition_integration_spec.rb
+++ spec/word_definition_integration_spec.rb
@@ -3,12 +3,12 @@ require('./app')
 Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
 
-describe('the dealership path', {:type => :feature}) do
+describe('the definition path', {:type => :feature}) do
   it('goes to the add-word page and lets user add word') do
     visit('/')
     click_link('Add New Words')
     fill_in('word_name', :with => 'anything')
-    click_button('Add Word')
-    expect(page).to have_content('Added!')
+    click_button("Add word")
+    expect(page).to have_content("Added")
   end
 end
