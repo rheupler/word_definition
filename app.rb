d4205e962f2983380ff6d51191ce04dd4678d61d
Mike Goren <mgoren@mortalwombat.net>
2015-08-16 14:52:36 -0700

    code review feedback

--- app.rb
+++ app.rb
@@ -8,26 +8,26 @@ get("/") do
   erb(:index)
 end
 
-get('/definitions') do
-  @definitions = Definition.all()
-  erb(:definitions)
-end
+# get('/definitions') do
+#   @definitions = Definition.all()
+#   erb(:definitions)
+# end
 
-get('/definition/new') do
-  erb(:definitions_form)
-end
+# get('/definition/new') do
+#   erb(:definitions_form)
+# end
 
-post('/defintions') do
-  definition = params.fetch("definition")
-  definition = Definition.new(definition)
-  definition.save()
-  erb(:success)
-end
+# post('/defintions') do
+#   definition = params.fetch("definition")
+#   definition = Definition.new(definition)
+#   definition.save()
+#   erb(:success)
+# end
 
-get('/definitions/:id') do
-  @definition = Definition.find(params.fetch("id").to_i())
-  erb(:definition)
-end
+# get('/definitions/:id') do
+#   @definition = Definition.find(params.fetch("id").to_i())
+#   erb(:definition)
+# end
 
 get("/words/new") do
   erb(:words_form)
@@ -40,15 +40,15 @@ end
 
 post("/words") do
   word_name = params.fetch("word_name")
-  Word.new(word_name).save()
-  @words = Word.all()
-  erb(:success)
+  word = Word.new(word_name)
+  word.save()
+  redirect "/words/#{word.id()}"
 end
 
-get("/definitions/:id") do
-  @definition = Definition.find(params.fetch("id").to_i())
-  erb(:definition)
-end
+# get("/definitions/:id") do
+#   @definition = Definition.find(params.fetch("id").to_i())
+#   erb(:definition)
+# end
 
 get("/words/:id") do
   @word = Word.find(params.fetch("id").to_i())
@@ -62,9 +62,10 @@ end
 
 post("/definitions") do
   definition = params.fetch("definition")
-  @definition = Definition.new(definition)
-  @definition.save()
-  # @word = Word.find(params.fetch("word_id").to_i())
-  # @word.add_definition(@definition)
-  erb(:success)
+  definition = Definition.new(definition)
+  # @definition.save() # for this exercise definition only needs to be added to word, not saved on its own
+  word = Word.find(params.fetch("word_id").to_i())
+  word.add_definition(definition)
+  # erb(:success)
+  redirect "/words/#{word.id()}"
 end
--- lib/definition.rb
+++ lib/definition.rb
@@ -1,39 +1,41 @@
 class Definition
 
-  @@definitions = []
+  # @@definitions = []
 
   def initialize(definition)
     @definition = definition
-    @id = @@definitions.length().+(1)
+    # @id = @@definitions.length().+(1)
   end
 
   def definition
     @definition
   end
 
-  def id
-    @id
-  end
+  # def id
+  #   @id
+  # end
 
-  define_singleton_method(:all) do
-    @@definitions
-  end
+  # define_singleton_method(:all) do
+  #   @@definitions
+  # end
 
-  def save
-    @@definitions.push(self)
-  end
+  # def save
+  #   @@definitions.push(self)
+  # end
 
-  define_singleton_method(:clear) do
-    @@definitions = []
-  end
+  # define_singleton_method(:clear) do
+  #   @@definitions = []
+  # end
 
-  define_singleton_method(:find) do |identification|
-    found_definition = nil
-    @@definitions.each() do |definition|
-      if definition.id().eql?(identification.to_i())
-        found_definition = definition
-      end
-    end
-    found_definition
-  end
+  # define_singleton_method(:find) do |identification|
+  #   found_definition = nil
+  #   @@definitions.each() do |definition|
+  #     if definition.id().eql?(identification.to_i())
+  #       found_definition = definition
+  #     end
+  #   end
+  #   found_definition
+  # end
 end
+
+# removed variables and methods not needed for this exercise
